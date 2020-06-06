9ed0c20480aefae064d02a42dee4b18202ce6a0c
Tiago Cabral <tiagorc.cabral@gmail.com>
2020-06-06 14:29:45 -0300

    Adds countries dropdown and enhances top banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:codvid_19/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: kBackgroundColor,
         fontFamily: "Poppins",
-        textTheme: TextTheme(body1: TextStyle(color: kBodyTextColor)),
+        textTheme: TextTheme(bodyText2: TextStyle(color: kBodyTextColor)),
         primarySwatch: Colors.blue,
       ),
       home: HomeScreen(),
@@ -30,6 +31,7 @@ class HomeScreen extends StatelessWidget {
           ClipPath(
             clipper: MyClipper(),
             child: Container(
+              padding: EdgeInsets.only(left: 40, top: 50, right: 20),
               height: 350,
               width: double.infinity,
               decoration: BoxDecoration(
@@ -41,8 +43,103 @@ class HomeScreen extends StatelessWidget {
                   image: AssetImage("assets/images/virus.png"),
                 ),
               ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: SvgPicture.asset("assets/icons/menu.svg"),
+                  ),
+                  SizedBox(height: 10),
+                  Expanded(
+                    child: Stack(
+                      children: <Widget>[
+                        SvgPicture.asset(
+                          "assets/icons/Drcorona.svg",
+                          width: 230,
+                          fit: BoxFit.fitWidth,
+                          alignment: Alignment.topCenter,
+                        ),
+                        Positioned(
+                          top: 20,
+                          left: 150,
+                          child: Text(
+                            "All you need \nis stay home.",
+                            style: kHeadingTextStyle.copyWith(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Container(),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            height: 60,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(25),
+              border: Border.all(
+                color: Color(0xFFE5E5E5),
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                SvgPicture.asset("assets/icons/maps-and-flags.svg"),
+                SizedBox(width: 20),
+                Expanded(
+                  child: DropdownButton(
+                    isExpanded: true,
+                    underline: SizedBox(),
+                    icon: SvgPicture.asset("assets/icons/dropdown.svg"),
+                    value: "Indonesia",
+                    items: ['Indonesia', 'Bangladesh', 'United States', 'Japan']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (value) {},
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Case update\n",
+                              style: kTitleTextstyle,
+                            ),
+                            TextSpan(
+                              text: "Newest update June 6th",
+                              style: TextStyle(
+                                color: kTextLightColor,
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ))
         ],
       ),
     );
